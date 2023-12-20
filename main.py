88ad052c07324350f7c1d48ddbac7dfee8ce566f
Mahsa Beigi <msbeigi@gmail.com>
2023-12-20 01:10:48 +0000

    Add and insert data

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import snowflake.snowpark as snowpark
-from snowflake.snowpark.functions import col
-from snowflake.snowpark.types import StructField, StructType, IntegerType, StringType, VariantType
-from snowflake.snowpark.session import Session
-import snowflake.connector
-from snowflake.connector.pandas_tools import write_pandas
-import getpass
-import pandas as pd
-import json
-from data.snowflake_utility import setup,create_table_edos,insert_file_odes,load_csv_file
+from data.snowflake_utility import insert_file_odes
 
 if (__name__=='__main__'):
-    # create_table_edos()
-    # insert_file_odes()
     insert_file_odes()
-    # print(d.iloc[:1])
 
 
