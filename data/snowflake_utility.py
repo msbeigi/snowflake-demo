b51c65ef919741575f1e9299a54bd39b4ba19747
Mahsa Beigi <msbeigi@gmail.com>
2023-12-20 01:06:45 +0000

    Add and insert data

--- data/snowflake_utility.py
+++ data/snowflake_utility.py
@@ -7,40 +7,85 @@ from snowflake.connector.pandas_tools import write_pandas
 import getpass
 import pandas as pd
 import json
+import os
+
+CONFIG={}
 
 
+def setup():
+    with open('./config.json') as f:
+        config_data=json.load(f)
+    snowflake_config=config_data.get('snowflake',{})
+    keys = ['user', 'password', 'account', 'warehouse', 'database', 'schema', 'role']
+
+    for key in keys:
+        CONFIG[key] = snowflake_config.get(key)
+    
+    return CONFIG#snowflake_config.get('user')
+    
 
 def connector():
-    
+    setup()
     con = snowflake.connector.connect(
-    user='msbeigi',
-    password='Ghi9rail755',
-    account='xcyevyt-po65493',
-    warehouse='COMPUTE_WH',
-    database='GIT',
-    schema='PUBLIC',
-    role='ACCOUNTADMIN'
+    user=CONFIG['user'],
+    password=CONFIG['password'],
+    account=CONFIG['account'],
+    warehouse=CONFIG['warehouse'],
+    database=CONFIG['database'],
+    schema=CONFIG['schema'],
+    role=CONFIG['role']
     )
     
     return con
 
 
 def create_table_edos():
-    df_comments=pd.read_csv('sample_comments.csv')
     con=connector()
     cur=con.cursor()
-    # create_command='CREATE TABLE "Odes_Comments" ( "rewire_id" Varchar(30),"text" Varchar(250),"label_sexist" Vachar(30),"label_category" Varchar(30),"label_vector" Varchar(15),"split" Varchar(8),"token" Varchar(30),"target" Varchar(30),"keyword" Varchar(30));'
-    create_command='CREATE TABLE "Odes_Comments" (  \
-                            "rewire_id" VARCHAR,    \
-                            "text" VARCHAR,    \
-                            "label_sexist" VARCHAR,    \
-                            "label_category" VARCHAR,    \
-                            "label_vector" VARCHAR,    \
-                            "split" VARCHAR,    \
-                            "token" VARCHAR,    \
-                            "target" VARCHAR,    \
-                            "keyword" VARCHAR    \
-                        );'
 
-    cur.execute(create_command)
+    table_exists_query = f"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{CONFIG['schema']}' AND TABLE_NAME = 'Odes_Comments'"
+    cur.execute(table_exists_query)
+    table_exists = cur.fetchone()[0]
+
+    if table_exists == 0:
+        create_command='CREATE TABLE "Odes_Comments" (  \
+                                "rewire_id" VARCHAR,    \
+                                "text" VARCHAR,    \
+                                "label_sexist" VARCHAR,    \
+                                "label_category" VARCHAR,    \
+                                "label_vector" VARCHAR,    \
+                                "split" VARCHAR,    \
+                                "token" VARCHAR,    \
+                                "target" VARCHAR,    \
+                                "keyword" VARCHAR    \
+                            );'
+
+        cur.execute(create_command)
+        print("Table 'Odes_Comments' created.")
+    else:
+        print("Table 'Odes_Comments' exists.")
+    cur.close()
+    con.close()
+
+
+def load_csv_file():
+    file_path = './data/sample_comments.csv'
+    df = pd.read_csv(file_path)
+
+    return df
+
+def insert_file_odes():
+    df_comments=load_csv_file()
+    conn=connector()
+    cur=conn.cursor()
+
+    select_command='SELECT * FROM "Odes_Comments"'
+    cur.execute(select_command)
+    data=cur.fetchall()
     
+    if not data:
+        # print(len(pd.DataFrame(data)))
+        write_pandas(conn,df_comments,table_name='Odes_Comments')
+
+    cur.close()
+    conn.close()
--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from snowflake.connector.pandas_tools import write_pandas
 import getpass
 import pandas as pd
 import json
-
+from data.snowflake_utility import setup,create_table_edos,insert_file_odes,load_csv_file
 
 if (__name__=='__main__'):
-
+    # create_table_edos()
+    # insert_file_odes()
+    insert_file_odes()
+    # print(d.iloc[:1])
 
 
